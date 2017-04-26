f063886d1d4f1b5ac428d16b04584d9a60a49de4
User <user@Users-MacBook-Pro.local>
2017-04-26 17:24:34 +0100

    added albums table

---
+++ models/album.rb
@@ -0,0 +1,34 @@
+require('pg')
+require_relative ('../db/sql_runner.rb')
+require_relative('../models/artist')
+require_relative('../models/album')
+
+class Album
+
+  attr_accessor :title, :genre
+  attr_reader :id, :artist_id
+
+  def initialize(options)
+    @title = options['title']
+    @genre = options['genre']
+    @id = options['id'].to_i if options['id']
+    @artist_id = options['artist_id'].to_i() if options['artist_id']
+  end
+
+  def save()
+    sql = "
+    INSERT INTO albums (
+      title,
+      genre,
+      artist_id
+    ) VALUES (
+      '#{ @title }',
+      '#{ @genre }',
+       #{ @artist_id }
+    )
+    RETURNING id;"
+    result = SqlRunner.run(sql)
+    @id = result[0]["id"].to_i
+  end
+
+end
